30cf834fbb08a5cde5436ccbab54fa8a6aa8cfa4
Soumyartho <banerjeesoumyartho@gmail.com>
2025-09-07 08:11:28 +0530

    final

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-    from app import create_app
-    app = create_app()
-    ```
-
-#### **Step 2: Push Your Project to GitHub**
-Render deploys directly from a GitHub repository. If you haven't done so already, you need to upload your project.
-
-1.  Create a new, empty repository on your GitHub account.
-2.  In your VS Code terminal (from the main project folder), run the following commands to upload your entire project:
-    ```bash
-    git init
-    git add .
-    git commit -m "Prepare for deployment"
-    git branch -M main
-    git remote add origin <Your_GitHub_Repository_URL>
-    git push -u origin main
-    
-
-=======
-from backend.app import app as application
+from app import app as application
 
 if __name__ == "__main__":
     application.run()
->>>>>>> 6f70e1bce87324f6cd47a820999c312c3500f292
